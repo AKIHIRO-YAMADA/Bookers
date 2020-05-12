6313cb6b911de620ce3101b095fe9be9353d5fd1
AKIHIRO-YAMADA <daa.xx.com.akki@gmail.com>
2020-05-12 22:57:57 +0900

    Start

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+  def index
+      @books = Book.all
+      @books = Book.new
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def create
+  	 book = Book.new(book_params)
+  	 book.save
+  	 redirect_to books_path
+  end
+  def edit
+  
+  end
+  
+  private
+  def book_params
+  	params.require(:blog).permit(:title, :body)
+  end
+
+end
