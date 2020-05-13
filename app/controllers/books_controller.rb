11067075636cc212edecee989417d507a7646bf1
AKIHIRO-YAMADA <daa.xx.com.akki@gmail.com>
2020-05-13 22:57:50 +0900

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def index
       @books = Book.all
-      @books = Book.new
+      @book = Book.new
   end
 
   def show
@@ -11,15 +11,26 @@ class BooksController < ApplicationController
   def create
   	 book = Book.new(book_params)
   	 book.save
-  	 redirect_to books_path
+  	 redirect_to book_path(book.id)
   end
   def edit
-  
+  	  @book = Book.find(params[:id])
+
   end
-  
+  def update
+  	  book =Book.find(params[:id])
+      book.update(book_params)
+      redirect_to book_path(book)
+  end
+  def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
+  end
+
   private
   def book_params
-  	params.require(:blog).permit(:title, :body)
+  	params.require(:book).permit(:title, :body)
   end
 
 end
